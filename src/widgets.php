80106e77bd47d03b152f107d7f068e28d94fb351
J. Marcelo Aviles Paco <marcelo@MacBook-Pro-de-Marcelo.local>
2023-05-19 12:23:08 -0400

    Added widget parameter to set control name

--- src/widgets.php
+++ src/widgets.php
@@ -4,13 +4,13 @@ namespace SinticBolivia\MonoInvoicesApi;
 use SinticBolivia\MonoInvoicesApi\Classes\MonoInvoicesApi;
 
 if( !function_exists('siat_widget_unidades_medida')):
-function siat_widget_unidades_medida(MonoInvoicesApi $api, $selected = '')
+function siat_widget_unidades_medida(MonoInvoicesApi $api, $selected = '', $name = 'unidad_medida_siat')
 {
 	$res = $api->unidadesMedida();
 	?>
 	<div class="mb-3">
 		<label>Unindad de Medida SIAT</label>
-		<select name="unidad_medida_siat" class="form-control form-select">
+		<select name="<?php print $name ?>" class="form-control form-select">
 			<option value="">-- unidad medida --</option>
 			<?php foreach($res->data->RespuestaListaParametricas->listaCodigos as $um): ?>
 			<option value="<?php print $um->codigoClasificador ?>" <?php print $selected == $um->codigoClasificador ? 'selected' : '' ?>>
@@ -23,13 +23,13 @@ function siat_widget_unidades_medida(MonoInvoicesApi $api, $selected = '')
 }
 endif;
 if( !function_exists('siat_widget_actividades')):
-function siat_widget_actividades(MonoInvoicesApi $api, $selected = '')
+function siat_widget_actividades(MonoInvoicesApi $api, $selected = '', $name = 'actividad_economica_siat')
 {
 	$res = $api->actividades();
 	?>
 	<div class="mb-3">
 		<label>Actividad Economica SIAT</label>
-		<select id="actividad_economica_siat" name="actividad_economica_siat" class="form-control form-select" data-selected="<?php print $selected ?>">
+		<select id="<?php print $name ?>" name="<?php print $name ?>" class="form-control form-select" data-selected="<?php print $selected ?>">
 			<option value="">-- actividad economica --</option>
 			<?php foreach($res->data->RespuestaListaActividades->listaActividades as $a): ?>
 			<option value="<?php print $a->codigoCaeb ?>" <?php print $selected == $a->codigoCaeb ? 'selected' : '' ?>>
@@ -42,19 +42,14 @@ function siat_widget_actividades(MonoInvoicesApi $api, $selected = '')
 }
 endif;
 if( !function_exists('siat_widget_productos')):
-function siat_widget_productos(MonoInvoicesApi $api, $selected = '', $filter_actividad = null)
+function siat_widget_productos(MonoInvoicesApi $api, $selected = '', $name = 'codigo_producto_siat')
 {
 	$res = $api->productosServicios();
 	?>
 	<div class="mb-3">
 		<label>Codigo Producto SIAT</label>
-		<select id="codigo_producto_siat" name="codigo_producto_siat" class="form-control form-select" data-selected="<?php print $selected ?>">
+		<select id="<?php print $name ?>" name="<?php print $name ?>" class="form-control form-select" data-selected="<?php print $selected ?>">
 			<option value="">-- producto sin --</option>
-			<?php /*foreach($res->data->RespuestaListaProductos->listaCodigos as $p): if( $filter_actividad && $filter_actividad != $p->codigoActividad ) continue; ?>
-			<option value="<?php print $p->codigoProducto ?>" <?php print $selected == $p->codigoProducto ? 'selected' : '' ?>>
-				<?php print $p->descripcionProducto ?>
-			</option>
-			<?php endforeach;*/ ?>
 		</select>
 	</div>
 	<script>
