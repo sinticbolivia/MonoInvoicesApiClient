877224c328f4fdda666b000aa0863c53418b3f97
J. Marcelo Aviles Paco <marcelo@MacBook-Pro-de-Marcelo.local>
2023-11-22 22:17:46 -0400

    Fixed migration directories routes

--- src/Classes/Migrator.php
+++ src/Classes/Migrator.php
@@ -37,12 +37,13 @@ class Migrator
 	}
 	public function migrateCustomers(int $destUserId)
 	{
-		print "MIGRANDO USUARIOS\n";
+		print "MIGRANDO CLIENTES\n";
 		$page = 1;
 		$limit = 50;
 		$res = $this->api->listadoClientes($page, $limit);
 		$total_pages = $res->getHeader('Total-Pages', 1);
 		$total_rows = $res->getHeader('Total-Rows', 1);
+		echo "TOTAL CLIENTES: {$total_rows}\n";
 		$this->persistCustomers($res->json()->data, $destUserId);
 		if( $total_pages > 1 )
 		{
@@ -70,6 +71,7 @@ class Migrator
 			$customer['user_id'] = $destUserId;
 			$meta = $cust->meta;
 			unset($customer['customer_id'], $customer['last_modificacion_data'], $customer['creation_date'], $customer['meta']);
+			echo "MIGRANDO CLIENTE: {$cust->last_name}\n";
 			$dbh->Insert('mb_customers', $customer);
 		}
 	}
@@ -78,8 +80,10 @@ class Migrator
 		print "MIGRANDO PRODUCTOS...\n";
 		$limit			= 50;
 		$products 		= $this->api->listadoProductos(1, $limit);
+		//print_r($this->api->lastResponse->rawBody);
 		$total_pages	= $this->api->lastResponse->getHeader('Total-Pages', 1);
-		$total_rows		= $this->api->lastResponse->getHeader('Total-Rows', 1);
+		$total_rows		= $this->api->lastResponse->getHeader('Total-Items', 1);
+		echo "TOTAL PRODUCTOS: {$total_rows}\n";
 		$this->persistProducts($products, $destUserId);
 		if( $total_pages > 1 )
 		{
@@ -99,6 +103,7 @@ class Migrator
 			$lprod = Product::GetBy('extern_id', $prod->id, true);
 			if( $lprod )
 				continue;
+			echo "MIGRANDO PRODUCTO: {$prod->product_name}\n";
 			$mprod = (array)$prod;
 			$mprod['extern_id'] = $prod->id;
 			$mprod['user_id'] 	= $destUserId;
@@ -110,13 +115,15 @@ class Migrator
 	}
 	public function migrateInvoices(int $destUserId)
 	{
+		echo "MIGRANDO FACTURAS\n";
 		$page 			= 1;
 		$limit			= 100;
 		$invoices 		= $this->api->listadoFacturas($page, $limit);
 		$total_pages 	= $this->api->lastResponse->getHeader('Total-Pages', 1);
-		$total_rows 	= $this->api->lastResponse->getHeader('Total-Rows', 1);
-		//print_r($this->api->lastResponse);
+		$total_rows 	= $this->api->lastResponse->getHeader('Total-Items', 1);
+		//print_r($this->api->lastResponse->rawBody);die;
 		//print_r($res);
+		echo "TOTAL FACTURAS: {$total_rows}\n";
 		$this->persistInvoices($invoices, $destUserId);
 		if( $total_pages > 1 )
 		{
@@ -144,12 +151,15 @@ class Migrator
 			if( $linvoice )
 				continue;
 			print "Migration invoice ID: {$invoice->invoice_id}\n";
+			$meta = $invoice->meta;
 			$minvoice = (array)$invoice;
 			$minvoice['extern_id'] 		= $invoice->invoice_id;
 			$minvoice['customer_id'] 	= $customer->customer_id;
 			$minvoice['user_id']		= $destUserId;
 			$minvoice['last_modification_date'] = $minvoice['creation_date'] = date('Y-m-d H:i:s');
-			unset($minvoice['invoice_id'], $minvoice['siat_url'], $minvoice['print_url'], $minvoice['sector'], $minvoice['leyenda']);
+			$minvoice['data'] = (is_object($invoice->data) || is_array($invoice->data)) ? json_encode($invoice->data) : $invoice->data;
+			//print_r($minvoice);die;
+			unset($minvoice['invoice_id'], $minvoice['siat_url'], $minvoice['print_url'], $minvoice['sector'], $minvoice['leyenda'], $minvoice['meta'], $minvoice['items']);
 			$id = $dbh->Insert('mb_invoices', $minvoice);
 			if( $id <= 0 )
 				continue;
@@ -160,6 +170,7 @@ class Migrator
 				unset($mitem['item_id']);
 				$mitem['last_modification_date'] = $mitem['creation_date'] = date('Y-m-d H:i:s');
 				$mitem['invoice_id'] = $id;
+				$mitem['data'] = (is_object($item->data) || is_array($item->data)) ? json_encode($item->data) : $item->data;
 				if( $item->product_id > 0 )
 				{
 					$lprod = Product::GetBy('extern_id', $item->product_id, true);
--- test/migracion.php
+++ test/migracion.php
@@ -1,17 +1,16 @@
 <?php
 define('SB_FRAMEWORK_PATH', dirname(dirname(__DIR__)) . '/SBFramework');
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+//require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/src/autoload.php';
 //##include SBFramework init
 require_once SB_FRAMEWORK_PATH . '/init.php';
 use SinticBolivia\MonoInvoicesApi\Classes\Migrator;
 use SinticBolivia\MonoInvoicesApi\Classes\ExceptionApi;
 
-
-
-$servidor = 'https://facturacion.1bytebo.net';
-$usuario = '1bytev2';
-$pwd = '1bytev2';
-$for_user_id = 16;
+$servidor 		= 'https://facturacion.1bytebo.net';
+$usuario 		= '1bytev2';
+$pwd 			= '1bytev2';
+$for_user_id 	= 16;
 try 
 {
 	$migrator = new Migrator($servidor, $usuario, $pwd);
